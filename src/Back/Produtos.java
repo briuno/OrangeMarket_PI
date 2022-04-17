c113632fc669917c796d0508d75adc8dad3cdc0e
paiottig <gabrielpaiotti01@gmail.com>
2022-04-17 17:13:26 -0300

    Inicio da inserção de Arrays

---
+++ src/Back/Produtos.java
@@ -0,0 +1,20 @@
+package Back;
+
+
+public class Produtos {
+    public String chocolate(){
+        String[] chocolates1 = {"Chocolate","4.00","Extra","12.3Km"};
+        String[] chocolates2 = {"Chocolate","7.00","Extra","13.0Km"};
+        String[] chocolates3 = {"Chocolate","8.00","Roldão","12.3Km"};
+        return (chocolates1[0]);
+    }
+    
+    
+    public String chocolateatac(){
+        String[] chocolatesatacado1 = {"Chocolate","4.00","Extra","12.3Km"};
+        String[] chocolatesatacado2 = {"Chocolate","7.00","Extra","13.0Km"};
+        String[] chocolatesatacado3 = {"Chocolate","8.00","Roldão","12.3Km"};
+        return (chocolatesatacado1[0]);
+    }
+    
+}
